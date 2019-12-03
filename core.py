5de160a3d62bdc41644971f096a041d08dc178e5
ngkame <48927124+ngkame@users.noreply.github.com>
2019-12-03 09:22:43 +0300

    date coll optimization

--- core.py
+++ core.py
@@ -22,8 +22,8 @@ def to_bigq(r, ds, tbl):
 
 def load_from_appm(xtable, xfields, xdate_since, xdate_until, xname):
     PARAMS = {'application_id': LC.APPLICATION_ID ,
-            'date_since': xdate_since + ' 00:00:00',
-            'date_until': xdate_until + ' 23:59:59',
+            'date_since': xdate_since ,
+            'date_until': xdate_until ,
             'date_dimension': 'default',
             'use_utf8_bom': 'true',
             'fields': xfields}
@@ -80,17 +80,17 @@ def date_coll(first_time=True):
     NOW_DATE = datetime.datetime.today()
     if first_time:
         CURSOR_DATE=datetime.datetime.today()-datetime.timedelta(days=LC.DATES['firsttime_depth'])
-        DLIST.append( [{'start': LC.DATES['first_date'], 'finish': CURSOR_DATE.strftime("%Y-%m-%d"), 'name': '_old'}])
+        DLIST.append( [{'start': LC.DATES['first_date']+ ' 00:00:00', 'finish': CURSOR_DATE.strftime("%Y-%m-%d")+ ' 23:59:59', 'name': '_old'}])
     else:
         CURSOR_DATE = NOW_DATE - datetime.timedelta(days=LC.DATES[f(NOW_DATE)])
 
 
         DLIST.append(
-            [{'start': CURSOR_DATE.strftime("%Y-%m-%d"), 'finish': CURSOR_DATE.strftime("%Y-%m-%d"), 'name': '_' + CURSOR_DATE.strftime("%Y%m%d")}])
+            [{'start': CURSOR_DATE.strftime("%Y-%m-%d")+ ' 00:00:00', 'finish': CURSOR_DATE.strftime("%Y-%m-%d")+ ' 23:59:59', 'name': '_' + CURSOR_DATE.strftime("%Y%m%d")}])
     while CURSOR_DATE < NOW_DATE:
         CURSOR_DATE += datetime.timedelta(days=1)
         fn = CURSOR_DATE.strftime("%Y%m%d")
-        DLIST.append({'start': CURSOR_DATE.strftime("%Y-%m-%d"), 'finish': CURSOR_DATE.strftime("%Y-%m-%d"),
+        DLIST.append({'start': CURSOR_DATE.strftime("%Y-%m-%d")+ ' 00:00:00', 'finish': CURSOR_DATE.strftime("%Y-%m-%d")+ ' 23:59:59',
                           'name': '_' + fn})
 
     return
@@ -99,7 +99,7 @@ def date_coll(first_time=True):
 if __name__ == "__main__":
     print('started at', datetime.datetime.now())
 
-    date_coll(False)
+    date_coll(True)
     print(DLIST)
     #table_lister(date_coll(first_time=True))
     #to_bigq(etl_sessions(), 'appmetrica', 'sessions_last_etl')
